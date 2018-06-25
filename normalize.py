916d729e997e025d34ce3a2e5bd5738aac485ce5
TravisPace <40004711+TravisPace@users.noreply.github.com>
2018-06-25 00:02:27 -0500

    Update normalize.py

--- normalize.py
+++ normalize.py
@@ -1,9 +1,17 @@
 def infNorm(vector):
+  '''
+  This takes the infinity norm of the vector.
+  '''
   result = 0
+  #Result is equal to no integer.
   for i in range(len(vector)):
+  #For every element in the vector.
     if result <= abs(vector[i]):
+    #This takes each element of the vector and sees if it greater than or equal to the previous element in the vector list.
       result = abs(vector[i])
+      #If the result is greater than or equal to than the previous element, then the element becomes the result.
   return result
+  #This returns the integer as the result.
 
 def scalarMultVec(scalar,vector):
   A = [ ]
@@ -12,12 +20,18 @@ def scalarMultVec(scalar,vector):
   return A
 
 def normalize(vector):
+  '''
+  This function normalizes the vector
+  '''
   result = infNorm(vector)
   if result == 1 or result == 0:
     return vector
+  #If the result is equal to 1 or zero from the infinity norm function, then this will return the vector.
   scalar = 1/result
   B = scalarMultVec(scalar,vector)
+  #This takes the scalar multiplication vector function and runs it to get B, B is the vector and scalar is scalar.
   return B
+  #This returns the vector B.
 
   
 
